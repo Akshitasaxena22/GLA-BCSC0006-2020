a8114ef5b23e088b0b7d7a26f28ac0c83cda5d96
dbc2201 <divyanshb22@gmail.com>
2020-01-05 23:39:34 +0530

    add explanation comment

--- src/arrays/ArrayDemo.java
+++ src/arrays/ArrayDemo.java
@@ -9,6 +9,17 @@ package arrays;
 
 public class ArrayDemo {
     public static void main(String[] args) {
+        /*
+         * 1. Declare and initialize an array of 10 primitive integers
+         * Notice, the "[]" brackets are written after the data-type declaration,
+         * this is how Java expects programmers to write array notation.
+         *
+         * The "magic number" 10 inside the brackets on the right-hand side is
+         * the size of the array, this tells us how many elements can be stored inside
+         * the array. As a rule, the magic number should be used as little as possible in
+         * the code.
+         * */
+        int[] array = new int[10];
 
     }
 }
