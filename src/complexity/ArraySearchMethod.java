6955114acf43322616abe75a2ea51cc408ec0519
dbc2201 <divyanshb22@gmail.com>
2020-01-07 00:20:21 +0530

    add explanation code for search method

--- src/complexity/ArraySearchMethod.java
+++ src/complexity/ArraySearchMethod.java
@@ -8,6 +8,12 @@
 package complexity;
 
 public class ArraySearchMethod {
+    /*
+     * If the target is not present in the array, then the for loop body will be executed x.length
+     * times. If the target is present, it could be anywhere. If we consider the average over all cases
+     * where the target is present, then the loop body will execute x.length/2 times. Therefore, the
+     * total execution time is directly proportional to x.length .
+     * */
     public static int search(int[] array, int target) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == target) {
