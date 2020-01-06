9b3d8c4644df8c459221192fc5dd0fbcf80a2cc6
dbc2201 <divyanshb22@gmail.com>
2020-01-07 00:18:34 +0530

    create a sample method to search a target value in an integer array

--- src/complexity/ArraySearchMethod.java
+++ src/complexity/ArraySearchMethod.java
@@ -8,5 +8,12 @@
 package complexity;
 
 public class ArraySearchMethod {
-
+    public static int search(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i; // return the index at which the target was found
+            }
+        }
+        return -1;  //  return -1 if the target was not found
+    }
 }
